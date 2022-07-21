f6396fbf685738af522eccd920f31c0e97b891a0
KASHISH GUPTA <109479077+kashishguptaa1@users.noreply.github.com>
2022-07-21 22:35:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+if(!isset($_SESSION['userId'])){ header('location:login.php');}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Banking</title>
+  <?php require 'assets/autoloader.php'; ?>
+  <?php require 'assets/db.php'; ?>
+  <?php require 'assets/function.php'; ?>
+  
+</head>
+<body style="background:#b878d6;background-size: 100%">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+ <a class="navbar-brand" href="#">
+    <img src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+   <!--  <i class="d-inline-block  fa fa-building fa-fw"></i> --><?php echo bankname; ?>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">  <a class="nav-link" href="accounts.php">Accounts</a></li>
+      <li class="nav-item ">  <a class="nav-link" href="statements.php">Account Statements</a></li>
+      <li class="nav-item ">  <a class="nav-link" href="transfer.php">Funds Transfer</a></li>
+      <!-- <li class="nav-item ">  <a class="nav-link" href="profile.php">Profile</a></li> -->
+
+
+    </ul>
+    <?php include 'sideButton.php'; ?>
+    
+  </div>
+</nav><br><br><br>
+<div class="row w-100" >
+  <div class="col" style="padding: 22px;padding-top: 0">
+    <div class="jumbotron shadowBlack" style="padding: 25px;min-height: 241px;max-height: 241px">
+  <h4 class="display-5">Welecome to ALL CITIZENS BANK</h4>
+  <p  class="lead alert alert-warning"><b>Latest Notification:</b>
+
+  <?php 
+      $array = $con->query("select * from notice where userId = '$_SESSION[userId]' order by date desc");
+      if ($array->num_rows > 0)
+      {
+        $row = $array->fetch_assoc();
+        // {
+          echo $row['notice'];
+        // }
+      }
+      else
+        echo "<div class='alert alert-info'>Notice box empty</div>";
+     ?></p>
+  
+</div>
+        <div id="carouselExampleIndicators" class="carousel slide my-2 rounded-1 shadowBlack" data-ride="carousel" >
+          <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="images/1.jpg" alt="First slide" style="max-height: 250px">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/2.jpg" alt="Second slide" style="max-height: 250px">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/3.jpg" alt="Third slide" style="max-height: 250px">
+          </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+  </div>
+<div class="col">
+    <div class="row" style="padding: 22px;padding-top: 0">
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="https://streetfins.com/wp-content/uploads/2020/07/API-Integration-for-Cards-Echeck-ACH-Processing.jpg" style="max-height: 155px;min-height: 155px" alt="Card image cap">
+          <div class="card-body">
+            <a href="accounts.php" class="btn btn-outline-success btn-block">Account Summary</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1593182440959-9d5165b29b59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9uZXklMjB0cmFuc2ZlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="Card image cap" style="max-height: 155px;min-height: 155px">
+        <div class="card-body">
+          <a href="transfer.php" class="btn btn-outline-success btn-block">Transfer Money</a>
+         </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" style="padding: 22px">
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="https://thumbs.dreamstime.com/b/notification-alert-digital-icon-internet-network-concept-people-checking-80315233.jpg" style="max-height: 155px;min-height: 155px" alt="Card image cap">
+          <div class="card-body">
+            <a href="notice.php" class="btn btn-outline-primary btn-block">Check Notification</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadowBlack ">
+          <img class="card-img-top" src="https://media.istockphoto.com/photos/phone-and-email-icons-on-wooden-cubes-with-contact-us-text-on-blue-picture-id1271752802?k=20&m=1271752802&s=612x612&w=0&h=O_QjEVcEdtQUGcUkJ8q19S93FFHNfs6AVlceDiqQ_jY=" alt="Card image cap" style="max-height: 155px;min-height: 155px">
+        <div class="card-body">
+          <a href="feedback.php" class="btn btn-outline-primary btn-block">Contact Us</a>
+         </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
